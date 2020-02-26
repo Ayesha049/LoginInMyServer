d0e3f534c4db94be733ec19c942a6418a3492a52
Ayesha Siddiqua <1405049as@gmail.com>
2020-02-26 12:16:24 +0600

    my custom dialog is added in profile

--- app/src/main/java/com/ayeshapp/imageeditor/ProfileActivity.java
+++ app/src/main/java/com/ayeshapp/imageeditor/ProfileActivity.java
@@ -2,7 +2,12 @@ package com.ayeshapp.imageeditor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        showdialog();
+    }
+
+    public void showdialog()
+    {
+        final Dialog dialog = new Dialog(ProfileActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.my_dialog);
+
+        ImageButton camera = dialog.findViewById(R.id.camera);
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ProfileActivity.this,"camera",Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        ImageButton gallery = dialog.findViewById(R.id.gallery);
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ProfileActivity.this,"gallery",Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
     }
 }
